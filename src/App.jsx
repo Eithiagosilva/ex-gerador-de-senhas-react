504e1eefab857e2cb47a044a247051d50582e321
Eithiagosilva <thiago.sizaias@gmail.com>
2024-03-27 16:35:04 -0300

    atualização com input

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 function App() {
   const [password, setPassword] = useState("");
+  const [passwordSize, setPasswordSize] = useState(12);
   const [copyText, setCopyText] = useState("Copiar");
 
   function generate() {
     const characters =
       "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
-    const length = 12;
     let newPassword = "";
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length);
       newPassword += characters[position];
     }
@@ -25,7 +25,19 @@ function App() {
   return (
     <div>
       <h1>Gerador de senhas</h1>
-      <button onClick={generate}>Gerar!</button>
+      <div>
+        <label htmlFor="passwordSize">Tamanho: </label>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
+      <button onClick={generate}>
+        Gerar senha de {passwordSize} caracteres
+      </button>
       <button onClick={copyToClipboard}>{copyText}</button>
       <div>{password}</div>
     </div>
